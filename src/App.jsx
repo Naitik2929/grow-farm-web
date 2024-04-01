e6c9f6855be4f4e1d939b0a7703d41a3d8c0d5b2
vikrampatel0408 <vikrampatel2313>
2024-04-01 09:59:21 +0530

    feedback form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Navbar from './components/navbar'
 import './App.css'
+import Feedbackform from './components/feedback'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  </>
+   <Navbar></Navbar> 
+   <Feedbackform></Feedbackform>
+   </>
   )
 }
 
